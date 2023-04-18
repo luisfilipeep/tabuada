1af889de3dfa0c5fd3a5838013b870c05df7e151
luisfilipeep <115740255+luisfilipeep@users.noreply.github.com>
2023-04-18 16:09:06 -0300

    Add files via upload

---
+++ tabuada2/main.js
@@ -0,0 +1,9 @@
+const toggleBtn = document.getElementById('toggle-mode');
+const body = document.body
+toggleBtn.addEventListener('click', function() {
+body.classList.toggle('dark-mode')
+
+
+})
+
+
